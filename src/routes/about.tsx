59ed80ba1d131fba4018aa03fea0264525318dfc
Albert Byiringiro <byiringiroalbert@gmail.com>
2025-11-02 16:34:44 +0200

    feat: import the generated root tree and create a new router instance

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
-  component: RouteComponent,
+  component: About,
 })
 
-function RouteComponent() {
-  return <div>Hello "/about"!</div>
+function About() {
+  return <div className='p-2'>Hello from About!</div>
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Button } from '@/components/ui/button'
+
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
+  component: Index,
 })
 
-function RouteComponent() {
-  return <div>Hello "/"!</div>
+function Index() {
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <h1 className='text-4xl'>Hello World</h1>
+      <Button className='bg-amber-700'>Click me</Button>
+    </div>
+  )
 }
